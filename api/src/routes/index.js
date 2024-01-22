804d423e1eb14b843751d0b2318cbeb709ef2cb7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-22 19:12:05 -0400

    Use the mainRouter in our new routes file to be later invoked from app.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,30 +2,28 @@ const { Router } = require('express');
 
 const mainRouter = Router();
 
-module.exports = mainRouter;
-
-/*
-app.get('/', (req, res) => {
+mainRouter.get('/', (req, res) => {
     res.status(200).send('Ok!');
 });
 
-app.get('/users', (req, res) => {
+mainRouter.get('/users', (req, res) => {
     res.status(200).send('Estoy en /users');
 });
 
-app.get('/users/:id', (req, res) => {
+mainRouter.get('/users/:id', (req, res) => {
     res.status(200).send('Detalles del usuario ' + req.params.id);
 });
 
-app.post('/users', (req, res) => {
+mainRouter.post('/users', (req, res) => {
     res.status(200).send('Voy a crear un usuario');
 });
 
-app.get('/posts', (req, res) => {
+mainRouter.get('/posts', (req, res) => {
     res.status(200).send('Estoy en posts');
 });
 
-app.get('/posts/:id', (req, res) => {
+mainRouter.get('/posts/:id', (req, res) => {
     res.status(200).send('Detalles del post ' + req.params.id);
 });
-*/
+
+module.exports = mainRouter;
