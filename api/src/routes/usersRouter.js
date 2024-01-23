74d284616d7067d35b0d84fd67ea782fcc503500
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-22 22:02:07 -0400

    Refactoring routes in usersRouter.js

--- api/src/routes/usersRouter.js
+++ api/src/routes/usersRouter.js
@@ -2,15 +2,15 @@ const { Router } = require('express');
 
 const usersRouter = Router();
 
-usersRouter.get('/users', (req, res) => {
+usersRouter.get('/', (req, res) => {
     res.status(200).send('Estoy en /users');
 });
 
-usersRouter.get('/users/:id', (req, res) => {
+usersRouter.get('/:id', (req, res) => {
     res.status(200).send('Detalles del usuario ' + req.params.id);
 });
 
-usersRouter.post('/users', (req, res) => {
+usersRouter.post('/', (req, res) => {
     res.status(200).send('Voy a crear un usuario');
 });
 
