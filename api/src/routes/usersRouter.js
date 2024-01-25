ebec57f8d6997caf3c7e59228411d8880dc36566
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-25 17:35:37 -0400

    Implementation of handlers for getting and creating users in the users router

--- api/src/routes/usersRouter.js
+++ api/src/routes/usersRouter.js
@@ -2,16 +2,25 @@ const { Router } = require('express');
 
 const usersRouter = Router();
 
-usersRouter.get('/', (req, res) => {
-    res.send('NIY: ESTA RUTA TRAE LA INFO DE TODOS LOS USUARIOS');
-});
+const getUsersHandler = (req, res) => {
+    // Llamar a la función que trae todos los usuarios de la base de datos.
+    // Llamar a la función que obtenga los datos de una API externa.
+    // Unir los datos, unificando el formato.
+    // cuando tenga los datos, responder con los datos.               
+}
 
-usersRouter.get('/:id', (req, res) => {
+const getUserHandler = (req, res) => {
     res.send('NIY: ESTA RUTA TRAE LA INFO DE UN USUARIO DETERMINADO POR ID ' + req.params.id);
-});
+}    
 
-usersRouter.post('/', (req, res) => {
+const createUserHandler = (req, res) => {
     res.send('NIY: ESTA RUTA CREA UN NUEVO USUARIO');
-});
+}    
+
+usersRouter.get('/', getUsersHandler);
+
+usersRouter.get('/:id', getUserHandler);
+
+usersRouter.post('/', createUserHandler);
 
 module.exports = usersRouter;
