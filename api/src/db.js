128e97b0f661a06cbf0b74fd1811eb52d067ab47
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-26 13:54:09 -0400

    Updated db.js to use environment variables in the database connection

--- api/src/db.js
+++ api/src/db.js
@@ -1,9 +1,13 @@
 // Crear la conexion con la base de datos.
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('postgres://postgres:123456@localhost:5432/ecommerce');
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/repaso`);
 
 // Definir los modelos.
 // Relacionarlos.
 // Exportarlos.
 
+module.exports = { sequelize };
