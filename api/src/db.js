a12c21e5514f0ab60c8af7d9e806d5378ce76d62
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-26 13:37:01 -0400

    Create .env file to export environment variables to db.js file

---
+++ api/src/db.js
@@ -0,0 +1,9 @@
+// Crear la conexion con la base de datos.
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres://postgres:123456@localhost:5432/ecommerce');
+
+// Definir los modelos.
+// Relacionarlos.
+// Exportarlos.
+
